ffeffeb8878c79fbafc5614392fd00ca54c07bc4
daidaiyigithub <129919456+daidaiyigithub@users.noreply.github.com>
2023-04-05 11:20:11 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4!!!");
         System.out.println("hello git4!!!");
         System.out.println("hello git4!!!");
+        System.out.println("pull git4!!!");
     }
 }
